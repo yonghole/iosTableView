de3e7e1fae67aa009bb21c06aeeb286ba9696c37
YongSangHo <yonghole@ajou.ac.kr>
2021-02-09 00:42:48 +0900

    Initial Commit

---
+++ ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  CarrotMarket2
+//
+//  Created by 용상호 on 2021/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    
+    
+   @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // tableView의 delegate를 지금 이 ViewController로!!
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+
+
+}
+
+class Listcell: UITableViewCell{
+    @IBOutlet weak var ti: UILabel!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var location: UILabel!
+    
+    
+}
